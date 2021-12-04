025c88738df75e53d0b5444e09e161ec4fee7a50
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-12-04 20:57:54 +0000

    First tests.

---
+++ test/utils.ts
@@ -0,0 +1,7 @@
+import * as hre from "hardhat"
+
+export const evm_increaseTime = async (seconds: number) => {
+    await hre.ethers.provider.send('evm_increaseTime', [seconds]);
+    await hre.network.provider.send("evm_mine");
+  }
+  
