42713b64cd753b216e652bfeccd2a0c861147bc0
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-12-06 00:22:04 +0000

    - Added log balance to mine step.

--- scripts/crabada.ts
+++ scripts/crabada.ts
@@ -5,6 +5,7 @@ import { Contract } from "ethers";
 import { BigNumber } from "@ethersproject/bignumber";
 import { TransactionResponse } from "@ethersproject/abstract-provider";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { formatEther, formatUnits } from "ethers/lib/utils";
 
 const ONE_GWEI = 1000000000
 
@@ -18,6 +19,14 @@ const contractAddress = {
     tusToken: '0xf693248F96Fe03422FEa95aC0aFbBBc4a8FdD172',
     craToken: '0xa32608e873f9ddef944b24798db69d80bbb4d1ed',
 }
+
+const logBalance = async (hre: HardhatRuntimeEnvironment, signerAddress: string) => {
+    console.log(`AVAX Balance: ${ formatEther(await hre.ethers.provider.getBalance(signerAddress)) }`);
+}
+
+const logTokenBalance = async (token: Contract, symbol: string, signerAddress: string, decimals: number = 18) => {
+    console.log(`${symbol} Balance: ${ formatUnits(await token.balanceOf(signerAddress), decimals) }`);
+}
   
 export const mineStep = async (hre: HardhatRuntimeEnvironment, teamId: number, gasprice: number, wait: number, signer: SignerWithAddress | undefined) => {
 
@@ -27,9 +36,24 @@ export const mineStep = async (hre: HardhatRuntimeEnvironment, teamId: number, g
         hre.ethers.provider
     )
 
-    if (signer){
-        idleGame = idleGame.connect(signer)
-    }
+    if (!signer)
+        signer = hre.ethers.getSigners()[0]
+    
+    idleGame = idleGame.connect(signer)
+
+    const tusToken = new Contract(
+        contractAddress.tusToken,
+        abi.ERC20,
+        hre.ethers.provider
+    )
+  
+    const craToken = new Contract(
+        contractAddress.craToken,
+        abi.ERC20,
+        hre.ethers.provider
+    )
+  
+    const signerAddress = await signer.getAddress()
 
     const teamInfo = await idleGame.getTeamInfo(teamId)
     const { currentGameId } = teamInfo
@@ -44,15 +68,26 @@ export const mineStep = async (hre: HardhatRuntimeEnvironment, teamId: number, g
             return
         }
 
+        await logBalance(hre, signerAddress)
+
+        await logTokenBalance(tusToken, 'TUS', signerAddress)
+        await logTokenBalance(craToken, 'CRA', signerAddress)
+
         console.log(`closeGame(gameId: ${currentGameId})`);
         const transactionResponse: TransactionResponse = await idleGame.closeGame(currentGameId, {gasPrice: gasprice*ONE_GWEI})
-        console.log(`transaction: ${transactionResponse.hash}.`);        
+        console.log(`transaction: ${transactionResponse.hash}`);        
         await transactionResponse.wait(wait)
+
+        await logTokenBalance(tusToken, 'TUS', signerAddress)
+        await logTokenBalance(craToken, 'CRA', signerAddress)
+
     }
 
+    await logBalance(hre, signerAddress)
     console.log(`startGame(teamId: ${teamId})`);
     const transactionResponse: TransactionResponse = await idleGame.startGame(teamId, {gasPrice: gasprice*ONE_GWEI})
     console.log(`transaction ${transactionResponse.hash}.`);
     await transactionResponse.wait(wait)            
+    await logBalance(hre, signerAddress)
 
 }
