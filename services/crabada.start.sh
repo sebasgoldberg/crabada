05bdcfd9c22b90b93a4089e79f37472218bbeff2
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-12-19 12:30:20 +0000

    Services update

--- services/crabada.start.sh
+++ services/crabada.start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 . /home/cerebro/.nvm/nvm.sh
-npx hardhat minestep --network mainnet --wait 15 --gasprice 25 --teamid 3286
+npx hardhat minestep --network mainnet --wait 10 --gasprice 25 --teamid 3286
+npx hardhat minestep --network mainnet --wait 10 --gasprice 25 --teamid 3759
