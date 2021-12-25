80a886af2fedf505ac727c6869cbf8dded9f29d5
Sebastian Goldberg <sebas.goldberg@gmail.com>
2021-12-25 11:14:44 +0000

    Added team 3286 to service.

--- services/crabada.start.sh
+++ services/crabada.start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/cerebro/.nvm/nvm.sh
 npx hardhat minestep --network mainnet --minerteamid 3759 --attackercontract 0x019e96438ed58C7F18D799b7CC2006273F81318a --attackerteamid 3873 --wait 7
-#npx hardhat minestep --network mainnet --minerteamid 3286 --attackercontract 0x019e96438ed58C7F18D799b7CC2006273F81318a --attackerteamid 3873 --wait 7
+npx hardhat minestep --network mainnet --minerteamid 3286 --attackercontract 0x019e96438ed58C7F18D799b7CC2006273F81318a --attackerteamid 3873 --wait 7
