3ee67de7259f0380f1ff20bf6f71260a2ef92c3e
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-01-13 20:27:54 +0000

    New teams added.

--- services/crabada.loot.sh
+++ services/crabada.loot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /home/cerebro/.nvm/nvm.sh
-npx hardhat loot --network localmainnet --lootersteamsbyaccount '[[3286, 3759, 5032], [5355]]' --testmode false
+npx hardhat loot --network localmainnet --lootersteamsbyaccount '[[3286, 3759, 5032], [5355, 5357, 6152]]' --testmode false
 
