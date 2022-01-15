39b5cdb1782ab7ded438f348a9c1d40771f9cf8f
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-01-15 11:09:56 +0000

    - Modified service loot script to use environment variable to define teams per account.

--- services/crabada.loot.sh
+++ services/crabada.loot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 . /home/cerebro/.nvm/nvm.sh
-npx hardhat loot --network localmainnet --lootersteamsbyaccount '[[3286, 3759, 5032], [5355, 5357, 6152]]' --testmode false
+if [ -z "$LOOTERS_TEAMS_BY_ACCOUNT" ]
+then
+    LOOTERS_TEAMS_BY_ACCOUNT='[[3286, 3759, 5032], [5355, 5357, 6152]]'
+fi
+npx hardhat loot --network localmainnet --lootersteamsbyaccount "$LOOTERS_TEAMS_BY_ACCOUNT" --testmode false
 
