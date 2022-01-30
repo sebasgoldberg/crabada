5eea71c4c27ffd32262c74c33686ebbcc5cf2a32
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-01-30 09:05:50 +0000

    One account by node.

--- config/nodes.ts
+++ config/nodes.ts
@@ -29,10 +29,10 @@ const looter2: AccountConfig = {
 
 
 export const CONFIG_BY_NODE_ID: ConfigByNodeId = {
-    1: { accountsConfigs: [ looter2, main, looter1] },
-    2: { accountsConfigs: [ looter2, main, looter1 ] },
-    4: { accountsConfigs: [ looter2, main, looter1 ]},
-    5: { accountsConfigs: [ looter2, main, looter1 ] },
-    9: { accountsConfigs: [ looter2, main, looter1 ] },
-    11: { accountsConfigs: [ looter2, main, looter1 ]},
+    1: { accountsConfigs: [ main] },
+    2: { accountsConfigs: [  ] },
+    4: { accountsConfigs: [ looter2 ]},
+    5: { accountsConfigs: [  ] },
+    9: { accountsConfigs: [ looter1 ] },
+    11: { accountsConfigs: [  ]},
 }
