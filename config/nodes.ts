f98a178ec628279173d0e3fcb090ae1c1c747878
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-02-01 09:56:52 +0000

    Teams reorganization.

--- config/nodes.ts
+++ config/nodes.ts
@@ -19,7 +19,7 @@ export const main: AccountConfig = {
 
 export const looter1: AccountConfig = {
     accountIndex: 1,
-    teams: [5355, 5357, 6152]
+    teams: [5355, 5357, /*6152*/]
 }
 
 export const looter2: AccountConfig = {
