504d4f1da3208966f74ad144f3e3aa7a0408fcdc
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-03-17 14:28:29 +0000

    Web server on port 3000 on localhost.

--- tasks/captcha.ts
+++ tasks/captcha.ts
@@ -1044,7 +1044,7 @@ class AttackServer {
 
         this.attackExecutor = new AttackExecutor(hre)
         this.attackExecutor.beginAttackInterval()
-        this.app.listen(9001,'0.0.0.0')
+        this.app.listen(3000)
 
     }
 
