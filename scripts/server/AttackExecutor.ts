d1b98750b5146172023c8e2f318d3c4ebc6ee127
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-05-18 12:49:03 +0000

    Refactored servers code.

---
+++ scripts/server/AttackExecutor.ts
@@ -0,0 +1,129 @@
+import { BigNumber, Contract, ethers } from "ethers"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { getSignerForAddress } from "../../tasks/captcha"
+import { getCrabadaContracts } from "../crabada"
+
+interface AttackTransactionData{
+    game_id: any,
+    user_address: any,
+    team_id: any,
+    expire_time: any, 
+    signature: any,
+}
+
+interface AttackTransactionDataByGameId{
+    [game_id: string]: AttackTransactionData
+}
+
+export class AttackExecutor{
+
+    hre: HardhatRuntimeEnvironment
+    attackTransactionsDataByGameId: AttackTransactionDataByGameId = {}
+    idleGame: Contract
+    teamsThatPerformedAttack: number[] = []
+
+    constructor(hre: HardhatRuntimeEnvironment){
+        this.hre = hre
+    }
+
+    isTeamBusy(teamId: number): boolean{
+
+        if (this.teamsThatPerformedAttack.includes(teamId))
+            return true
+
+        const teamsAlreadyAttacking = []
+
+        for (const gameId in this.attackTransactionsDataByGameId){
+            const data = this.attackTransactionsDataByGameId[gameId]
+            teamsAlreadyAttacking.push(Number(data.team_id))
+        }
+
+        return teamsAlreadyAttacking.includes(teamId)
+    }
+
+    addAttackTransactionData(attackTransactionData: AttackTransactionData){
+        this.attackTransactionsDataByGameId[attackTransactionData.game_id] = attackTransactionData
+        this.setAddressRecentlyAttacked(attackTransactionData.user_address)
+    }
+
+    addressRecentlyAttackedByAdress: {
+        [addressInLowerCase: string]: boolean
+    } = {}
+
+    hasAddressRecentlyAttacked(address: string){
+        return this.addressRecentlyAttackedByAdress[address.toLowerCase()]
+    }
+
+    setAddressRecentlyAttacked(address: string){
+        this.addressRecentlyAttackedByAdress[address.toLowerCase()] = true
+        setTimeout(() => {
+            this.addressRecentlyAttackedByAdress[address.toLowerCase()] = false
+        }, 60_000)
+    }
+
+    async attackTransaction({user_address, game_id, team_id, expire_time, signature}: AttackTransactionData){
+
+        const { idleGame } = getCrabadaContracts(this.hre)
+
+        const signers = await this.hre.ethers.getSigners()
+
+        const looterSigner = getSignerForAddress(signers, user_address)
+
+        try {
+            console.log('looterSigner', looterSigner.address)
+            console.log('idleGame.attack(game_id, team_id, expire_time, signature)', game_id, team_id, expire_time, signature);
+            await idleGame.connect(looterSigner).callStatic["attack(uint256,uint256,uint256,bytes)"](
+                BigNumber.from(game_id), BigNumber.from(team_id), BigNumber.from(expire_time), signature, 
+                this.hre.crabada.network.getAttackOverride()
+            )
+            const txr: ethers.providers.TransactionResponse = await idleGame.connect(looterSigner)["attack(uint256,uint256,uint256,bytes)"](
+                BigNumber.from(game_id), BigNumber.from(team_id), BigNumber.from(expire_time), signature,
+                this.hre.crabada.network.getAttackOverride()
+            )
+            console.log('txr.hash', txr.hash);
+            this.teamsThatPerformedAttack.push(Number(team_id))
+            delete this.attackTransactionsDataByGameId[game_id]
+        } catch (error) {
+            console.error('Error trying to attack', String(error));
+            if ((+new Date()/1000)>Number(expire_time))
+                delete this.attackTransactionsDataByGameId[game_id]
+        }
+
+    }
+
+    hasTeamPendingAttack(teamId: any){
+        for (const gameId in this.attackTransactionsDataByGameId){
+            const { team_id } = this.attackTransactionsDataByGameId[gameId]
+            if (String(teamId) == String(teamId)){
+                return true
+            }
+        }
+        return false
+    }
+
+    beginAttackInterval(): NodeJS.Timer {
+
+        let attackInExecution = false
+
+        return setInterval(async ()=>{
+
+            if (attackInExecution)
+                return
+
+            attackInExecution = true
+
+            const gameIdsToAttack = Object.keys(this.attackTransactionsDataByGameId)
+
+            for (const gameId of gameIdsToAttack){
+                const attackTransactionData: AttackTransactionData = this.attackTransactionsDataByGameId[gameId]
+                await this.attackTransaction(attackTransactionData)
+            }
+
+            attackInExecution = false
+
+        },5_000)
+
+    }
+
+}
+
---
+++ scripts/server/AuthServer.ts
@@ -0,0 +1,132 @@
+import axios from "axios"
+import { ethers, Wallet } from "ethers"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { MAINNET_AVAX_MAIN_ACCOUNTS_PKS } from "../../hardhat.config"
+import { DEBUG } from "../api"
+
+export class AuthServer {
+
+    authenticateInterval: NodeJS.Timer = undefined
+    wallets: ethers.Wallet[]
+
+    accessTokenByAddress: {
+        [address: string]: string
+    } = {}
+
+    hre: HardhatRuntimeEnvironment
+
+    constructor(hre: HardhatRuntimeEnvironment){
+
+        this.hre = hre
+
+        this.wallets = this.hre.crabada.network.LOOT_CAPTCHA_CONFIG.players
+            .map(({signerIndex})=>signerIndex)
+            .map( index => new Wallet(MAINNET_AVAX_MAIN_ACCOUNTS_PKS[index]))
+
+        this.start()
+
+    }
+
+    start(retryMs=20_000){
+        if (this.authenticateInterval)
+            return
+        this.authenticateInterval = setInterval(()=>{
+            this.authenticateIfNotAuthenticated()
+        }, retryMs)
+    }
+
+    stop(){
+        if (this.authenticateInterval == undefined)
+            return
+        clearInterval(this.authenticateInterval)
+        this.authenticateInterval = undefined
+    }
+
+    getToken(address: string){
+        return this.accessTokenByAddress[address.toLowerCase()]
+    }
+
+    async authenticateIfNotAuthenticated(){
+
+        await Promise.all(
+
+            this.wallets.map( async(w) => {
+
+                const signedAddress = w.address.toLowerCase()
+    
+                if (this.getToken(signedAddress))
+                    return
+    
+                const timestamp = String(+new Date())
+
+                const message = `${signedAddress}_${timestamp}`
+                const signedMessage = await w.signMessage(message)
+                
+                console.log('Message to sign', message);
+                console.log('Signed message', signedMessage);
+    
+                const url = `${this.hre.crabada.network.getCrabadaApiBaseUrl()}/crabada-user/public/login-signature`
+    
+                const headers = {
+                    'authority': 'api.crabada.com',
+                    'pragma': 'no-cache',
+                    'cache-control': 'no-cache',
+                    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                    accept: 'application/json, text/plain, */*',
+                    'content-type': 'application/json',
+                    'sec-ch-ua-mobile': '?0',
+                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
+                    'sec-ch-ua-platform': '"Windows"',
+                    origin: this.hre.crabada.network.getOrigin(),
+                    'sec-fetch-site': 'same-site',
+                    'sec-fetch-mode': 'cors',
+                    'sec-fetch-dest': 'empty',
+                    'referer': this.hre.crabada.network.getReferer(),
+                    'accept-language': 'pt-BR,pt;q=0.9,es;q=0.8,en;q=0.7,de;q=0.6,en-US;q=0.5,he;q=0.4',
+                }
+    
+                try {
+
+                    DEBUG && console.log('POST', url);
+    
+                    const response = await axios.post(url, {
+                        address: signedAddress,
+                        sign: signedMessage,
+                        timestamp,
+                    },{
+                        headers
+                    })
+    
+    
+                    if (response.status == 200) {
+    
+                        const { result: { accessToken } } = response.data
+
+                        this.accessTokenByAddress[signedAddress] = accessToken
+
+                        console.log('Authentication succed for address', signedAddress, 'with access token', accessToken);
+    
+                    } else {
+                        throw({
+                            status: response.status,
+                            data: response.data
+                        })
+                    }
+    
+    
+                } catch (error) {
+    
+                    console.error('ERROR trying to authenticate address', w.address, String(error))
+                    error.response && console.error(error.response.data);
+    
+                }
+    
+            })
+        )
+
+
+
+    }
+
+}
+
