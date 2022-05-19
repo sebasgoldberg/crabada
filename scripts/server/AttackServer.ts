218c047c11181f7115f1410bcac840dc98a93ff9
Sebastian Goldberg <sebas.goldberg@gmail.com>
2022-05-19 13:45:09 +0000

    Revert "Disabled ONLY_ATTACK_TEAMS_THAT_PLAY_TO_LOOSE."
    This reverts commit bfb0034e48c6a1988b0bc539152c23f5ff85a0cc.

--- scripts/server/AttackServer.ts
+++ scripts/server/AttackServer.ts
@@ -396,7 +396,7 @@ export class AttackServer {
     }
 
     teamsThatPlayToLooseByTeamId: ITeamsThatPlayToLooseByTeamId = {}
-    static ONLY_ATTACK_TEAMS_THAT_PLAY_TO_LOOSE = false
+    static ONLY_ATTACK_TEAMS_THAT_PLAY_TO_LOOSE = true
 
     async initialize(){
         if (AttackServer.ONLY_ATTACK_TEAMS_THAT_PLAY_TO_LOOSE)
